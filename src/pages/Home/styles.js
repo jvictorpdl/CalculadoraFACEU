ad056046fd8144eb37e9ec6796f8ec4b26c2a89b
Joao Victor <jvictorpdantasl@gmail.com>
2023-06-23 23:28:47 -0300

    ...

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -7,6 +7,7 @@ export const Container = styled.section`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  /* background-color: #F4F7FE; */
 `;
 
 export const TitleCard = styled.h2`
